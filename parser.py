b78738b788e0574dccf881cc5c60f3bd2b134d67
i11exx <iraalexxan@gmail.com>
2021-09-22 00:21:04 +0300

    Output result as a string, parse_date method added

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 import bs4
 import requests
+import datetime
 import urllib.parse
 
 
@@ -11,6 +12,7 @@ class Parser:
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
             'Accept-Language': 'ru'
         }
+        self.adverts = []
 
     def get_page_text(self, page: int = None):
         params = {
@@ -37,6 +39,52 @@ class Parser:
         r = urllib.parse.urlparse(href)
         params = urllib.parse.parse_qs(r.query)
         return int(params['p'][0])
+
+    @staticmethod
+    def parse_date(item: str):
+        params = item.strip().split(' ')
+        if len(params) == 2:
+            day, time = params
+            if day == 'Сегодня':
+                date = datetime.date.today()
+            elif day == 'Вчера':
+                date = datetime.date.today() - datetime.timedelta(days=1)
+            else:
+                print('Не удаелось получить день:', item)
+                return
+
+            time = datetime.datetime.strptime(time, '%Н:%M').time()
+            return datetime.datetime.combine(date=date, time=time)
+
+        elif len(params) == 3:
+            day, month_ad, time = params
+            day = int(day)
+            month_map = {
+                'января': 1,
+                'ферваля': 2,
+                'марта': 3,
+                'апреля': 4,
+                'мая': 5,
+                'июня': 6,
+                'июля': 7,
+                'августа': 8,
+                'сентября': 9,
+                'октября': 10,
+                'ноября': 11,
+                'декабря': 12
+            }
+            month = month_map.get(month_ad)
+            if not month:
+                print('Не удалось получить месяц:', item)
+                return
+
+            today = datetime.datetime.today()
+            time = datetime.datetime.strptime(time, '%Н:%M')
+            return datetime.datetime(day=day, month=month, year=today.year, hour=time.hour, minute=time.minute)
+
+        else:
+            print('Не удалось разобрать формат:', item)
+            return
 
     def parse_advert(self, item):
         url_block = item.select_one(
@@ -89,9 +137,21 @@ class Parser:
             'div.iva-item-root-Nj_hb.photo-slider-slider-_PvpN.iva-item-list-H_dpX.iva-item-redesign-nV4C4.iva-item-responsive-gIKjW.items-item-My3ih.items-listItem-Gd1jN.js-catalog-item-enum')
         for advert in adverts_wrapper:
             block = self.parse_advert(item=advert)
-            print(block)
+            self.adverts.append(block)
 
     def parse_adverts(self):
         limit = self.get_pages_amount()
         for i in range(1, limit + 1):
             self.parse_page(page=i)
+
+        def get_price(ad):
+            words = ad.get('price').split()
+            if len(words) == 2:
+                return int(words[1])
+            else:
+                return int(words[0])
+
+        self.adverts.sort(key=get_price)
+
+        for i in self.adverts:
+            print(f'{i.get("title")}\t{i.get("price")} {i.get("currency")}\t{i.get("date")}\t{i.get("url")}')
